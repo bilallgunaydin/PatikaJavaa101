8808eba0ae89aa838e4b31c83a63e8ea94f25575
Bilal Günaydın <bilallgunaydin@gmail.com>
2022-05-08 22:13:01 +0300

    Add files via upload

---
+++ Transpoze/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int row = 2, column = 3;
+        int[][] matrix = { {1, 2, 3}, {4, 5, 6} };
+
+
+        display(matrix);
+
+        int[][] transpose = new int[column][row];
+        for(int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        display(transpose);
+    }
+
+    public static void display(int[][] matrix) {
+        System.out.println("The matrix is: ");
+        for(int[] row : matrix) {
+            for (int column : row) {
+                System.out.print(column + "    ");
+            }
+            System.out.println();
+        }
+    }
+}
